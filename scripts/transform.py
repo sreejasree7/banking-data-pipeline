fd8f137345f2722cdd02b9291209ad130148e126
sreejasree7 <sreejasree7@gmail.com>
2026-05-03 18:28:39 +1000

    Update transform.py

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+def transform_data():
+  print("Starting data tranformation...")
+
+  df = pd.read_csv("data/extracted_transactions.csv")
+
+  #Remove null values
+  df.dropna(inplace=True)
+
+  #Remove duplicates
+  df.drop_duplicates(inplace=True)
+
+  # Example transformation:
+  # Add fraud flag if transaction > 10000
+  df['fraud_flag']=df['trnaformation_amount'].apply(
+    lambda x: 'YES' if x > 10000 else 'NO'
+  )
+
+  df.to_csv("data/transformed_transactions.csv", index=False)
+
+  print(f"Transformed {len(df)} clean records")
+  print("Transformation completeds successfully")
