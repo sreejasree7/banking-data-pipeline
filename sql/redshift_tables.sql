bcb2876b0c2f12b220b85ad2f7d627fc9fd594ed
sreejasree7 <sreejasree7@gmail.com>
2026-05-03 18:37:36 +1000

    Update redshift_tables.sql

--- sql/redshift_tables.sql
+++ sql/redshift_tables.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS customer_transactions (
+  transaction_id VARCHAR(50),
+  custormer_id  VARCHAR(50),
+  transactions_date TIMESTAMP,
+  transaction_amount DECIMAL(18,2),
+  transaction_type VARCHAR(100),
+  fraud_flag VARCHAR(10)
+  );
